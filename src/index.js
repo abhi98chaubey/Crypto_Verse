57693d38383a0d9500750f6542740d700c042cab
Abhishek98chaubey <abhishek81logan@gmail.com>
2024-02-20 23:15:33 +0530

    ready for deplyment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // Import necessary dependencies
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
+
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 // Import your root component
@@ -9,15 +12,16 @@ import '@ant-design/cssinjs';
 import store from './app/store';
 
 // Render the root component in the 'root' element of your HTML
-ReactDOM.render(
-    <Router>
-      <Provider store = {store}>
-            <React.StrictMode>
-              <App />
-           </React.StrictMode>
-      </Provider>
-        
-    </Router>
- ,
-  document.getElementById('root')
+createRoot(document.getElementById('root')).render(
+  <Router>
+    <Provider store = {store}>
+          <React.StrictMode>
+            <App />
+         </React.StrictMode>
+    </Provider>
+      
+  </Router>
 );
+
+
+
